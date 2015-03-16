fce2e8cf99625fad2b2ced156e55a33338ee41d4
kinglin <lince110@163.com>
2015-03-16 09:28:24 +0800

    test of strconv

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
-// TestofGo project main.go
 package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	var orig string = "56"
+	// var an int
+	var newS string
+	// var err error
+
+	fmt.Printf("The size of ints is: %d\n", strconv.IntSize)
+	// anInt, err = strconv.Atoi(origStr)
+	an, err := strconv.Atoi(orig)
+	if err != nil {
+		fmt.Printf("orig %s is not an integer - exiting with error\n", orig)
+		return
+	}
+	fmt.Printf("The integer is %d\n", an)
+	an = an + 5
+	newS = strconv.Itoa(an)
+	fmt.Printf("The new string is: %s\n", newS)
 }
